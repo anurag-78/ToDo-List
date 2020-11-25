4d0923b15f043ee6468b3b5b138c1dbb0c3283a7
anurag diwedi <anuragdiwedi78@gmail.com>
2020-11-25 18:56:03 +0530

    make it better

--- app.js
+++ app.js
@@ -143,13 +143,8 @@ else
 
 
 });
-let port=process.env.PORT;
-if(port==null||port==""){
-  port=3000;
-}
 
 
-
-app.listen(port, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
